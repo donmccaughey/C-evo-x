59aee07f1dcf6b8edfb3a31d5ff6ec57ab2caf01
Don McCaughey <don@donm.cc>
2019-03-03 15:12:41 -0800

    Update file properties for the C# projects.
    Modify the `AssemblyInfo.cs` files for the `CevoDotNet.exe` and
    `Configurator.exe` projects to set the correct file details for C-evo-x
    1.2.1.

--- AI_Template/CevoDotNet/Properties/AssemblyInfo.cs
+++ AI_Template/CevoDotNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("C-evo .Net Loader")]
+[assembly: AssemblyTitle("C-evo-x .NET Loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("C-evo")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("C-evo-x")]
+[assembly: AssemblyCopyright("In the public domain")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
